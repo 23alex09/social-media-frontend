14a160823a0e6c462e67484b532699cef2d0a3a2
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2023-03-06 19:14:36 +0100

    Implemet Spinner component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+
+import { client } from "../client"
+import { MasonryLayout } from './MasonryLayout'
+import { Spinner } from './Spinner'
 
 export const Feed = () => {
+
+    const [ loading, setLoading ] = useState( false );
+
+    if ( loading ) return <Spinner message={ 'We are adding new ideas to your feed!' } />
+
     return (
         <div>Feed</div>
     )
